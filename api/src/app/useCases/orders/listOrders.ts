0aac0489f6ccfeb6e0a698b8c265761fce287224
marcos <dev.marcosanjos@gmail.com>
2025-01-09 12:41:12 -0300

    feat: Sort orders by creation date in listOrders use case

--- api/src/app/useCases/orders/listOrders.ts
+++ api/src/app/useCases/orders/listOrders.ts
@@ -3,7 +3,9 @@ import { Order } from "../../models/Order";
 
 export async function listOrders(req: Request, res: Response) {
   try {
-    const orders = await Order.find().populate("products.product");
+    const orders = await Order.find()
+      .sort({ createAt: -1 })
+      .populate("products.product");
 
     res.json(orders);
   } catch {
