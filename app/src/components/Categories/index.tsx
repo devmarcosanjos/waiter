5c517e8f4333299e8e9d1b470983b583de217c7d
marcos <dev.marcosanjos@gmail.com>
2025-01-12 09:21:39 -0300

    feat: Enhance Categories component with selection state and update styles for touch feedback

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { categories } from "../../mocks/categories";
 import { Text } from "../Text";
 import { Category, Icon } from "./styles";
 import { FlatList } from "react-native";
 
 export function Categories() {
+  const [selectedCategory, setSelectCategory] = useState("");
+
+  function handleSelectCategory(categoryId: string) {
+    setSelectCategory(categoryId);
+  }
+
   return (
     <FlatList
       horizontal
@@ -11,16 +18,20 @@ export function Categories() {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={(category) => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-          <Text size={14} weight="700">
-            {category.name}
-          </Text>
-        </Category>
-      )}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text size={14} weight="700" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
     />
   );
 }
--- app/src/components/Categories/styles.ts
+++ app/src/components/Categories/styles.ts
@@ -1,6 +1,9 @@
+import { Platform } from "react-native";
 import styled from "styled-components/native";
 
-export const Category = styled.View`
+const isAndroid = Platform.OS === "android";
+
+export const Category = styled.TouchableOpacity`
   align-items: center;
   margin-left: 24px;
 `;
@@ -14,4 +17,6 @@ export const Icon = styled.View`
   justify-content: center;
   align-items: center;
   margin-bottom: 8px;
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 0.1 : 1});
+  ${isAndroid && "elevation: 2;"}
 `;
