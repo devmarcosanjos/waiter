fad95e349cc0a7c72fc5b8522429435d55033b18
marcos <dev.marcosanjos@gmail.com>
2025-01-12 19:30:09 -0300

    feat: Implement Menu component to display a list of products with images and details

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,3 +1,30 @@
+import { FlatList } from "react-native";
+import { products } from "../../mocks/products";
+import { Text } from "../Text";
+import { Image, Product, ProductDetails } from "./styles";
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={(products) => products._id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <Image
+            source={{
+              uri: "https://732e-186-194-148-5.ngrok-free.app/uploads/1736432707539-quatro-queijos.png   ",
+            }}
+          />
+
+          <ProductDetails>
+            <Text>{product.name}</Text>
+            <Text>{product.description}</Text>
+            <Text>{product.price}</Text>
+          </ProductDetails>
+        </Product>
+      )}
+    />
+  );
 }
---
+++ app/src/components/Menu/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Image = styled.Image`
+  width: 120px;
+  height: 96px;
+`;
+export const ProductDetails = styled.View``;
+
+export const Product = styled.View``;
