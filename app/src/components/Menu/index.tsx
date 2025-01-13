b112efc9329c7332c45f68f32e217db21c4d8072
marcos <dev.marcosanjos@gmail.com>
2025-01-13 09:55:28 -0300

    feat: Enhance Menu component with product image path updates, improved styling, and currency formatting utility

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,27 +1,33 @@
 import { FlatList } from "react-native";
 import { products } from "../../mocks/products";
 import { Text } from "../Text";
-import { Image, Product, ProductDetails } from "./styles";
+import { Image, Product, ProductDetails, Separator } from "./styles";
+import { formatCurrency } from "../../utils/formatCurrency";
 
 export function Menu() {
   return (
     <FlatList
       data={products}
       style={{ marginTop: 32 }}
+      ItemSeparatorComponent={Separator}
       contentContainerStyle={{ paddingHorizontal: 24 }}
       keyExtractor={(products) => products._id}
       renderItem={({ item: product }) => (
         <Product>
           <Image
             source={{
-              uri: "https://732e-186-194-148-5.ngrok-free.app/uploads/1736432707539-quatro-queijos.png   ",
+              uri: `https://448a-186-194-148-5.ngrok-free.app/uploads/${product.imagePath}`,
             }}
           />
 
           <ProductDetails>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.price}</Text>
+            <Text weight="600">{product.name}</Text>
+            <Text color="#666" size={14} style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
           </ProductDetails>
         </Product>
       )}
