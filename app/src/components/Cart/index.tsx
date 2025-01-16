b711b0401dbd396036d26c09be110ddd26e5c964
marcos <dev.marcosanjos@gmail.com>
2025-01-16 11:47:38 -0300

    feat: Enhance Cart component with conditional rendering and improved total display

--- app/src/components/Cart/index.tsx
+++ app/src/components/Cart/index.tsx
@@ -1,5 +1,5 @@
 import { FlatList, TouchableOpacity } from "react-native";
-import { CartItem } from "../../types/cardItem";
+import { CartItem } from "../../types/cartItem";
 import {
   Action,
   Image,
@@ -18,62 +18,75 @@ import { MinusCircle } from "../Icons/MinusCircle";
 import { Button } from "../Button";
 
 interface CartProps {
-  cartItems: CartItem;
+  cartItems: CartItem[];
 }
 
 export function Cart({ cartItems }: CartProps) {
   return (
     <>
-      <FlatList
-        data={cartItems}
-        keyExtractor={(cartItems) => cartItems.product._id}
-        style={{
-          marginBottom: 20,
-          maxHeight: 150,
-        }}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item: cardItem }) => (
-          <Item>
-            <ProductContainer>
-              <Image
-                source={{
-                  uri: `https://448a-186-194-148-5.ngrok-free.app/uploads/${cardItem.product.imagePath}`,
-                }}
-              />
-              <QuantityContainer>
-                <Text size={14} color="#666">
-                  {cardItem.quantity} x
-                </Text>
-              </QuantityContainer>
-              <ProductDetails>
-                <Text size={14} weight="600">
-                  {cardItem.product.name}
-                </Text>
-                <Text size={14} color="#666" style={{ marginTop: 4 }}>
-                  {formatCurrency(cardItem.product.price)}
-                </Text>
-              </ProductDetails>
-            </ProductContainer>
-            <Action>
-              <TouchableOpacity style={{ marginRight: 24 }}>
-                <PlusCircle />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <MinusCircle />
-              </TouchableOpacity>
-            </Action>
-          </Item>
-        )}
-      />
+      {cartItems.length > 0 && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={(cartItems) => cartItems.product._id}
+          style={{
+            marginBottom: 20,
+            maxHeight: 150,
+          }}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: cardItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={{
+                    uri: `https://448a-186-194-148-5.ngrok-free.app/uploads/${cardItem.product.imagePath}`,
+                  }}
+                />
+                <QuantityContainer>
+                  <Text size={14} color="#666">
+                    {cardItem.quantity} x
+                  </Text>
+                </QuantityContainer>
+                <ProductDetails>
+                  <Text size={14} weight="600">
+                    {cardItem.product.name}
+                  </Text>
+                  <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                    {formatCurrency(cardItem.product.price)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
+              <Action>
+                <TouchableOpacity style={{ marginRight: 24 }}>
+                  <PlusCircle />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <MinusCircle />
+                </TouchableOpacity>
+              </Action>
+            </Item>
+          )}
+        />
+      )}
       <Summary>
         <TotalContainer>
-          <Text color="#666">Total</Text>
-          <Text size={20} weight="600">
-            {formatCurrency(120)}
-          </Text>
+          {cartItems.length > 0 ? (
+            <>
+              <Text color="#666">Total</Text>
+              <Text size={20} weight="600">
+                {formatCurrency(120)}
+              </Text>
+            </>
+          ) : (
+            <>
+              <Text color="#999">Seu Carrinho está vazio</Text>
+            </>
+          )}
         </TotalContainer>
 
-        <Button onPress={() => alert("Confimar pedido")}>
+        <Button
+          onPress={() => alert("Confimar pedido")}
+          disabled={cartItems.length === 0}
+        >
           Confimar pedido
         </Button>
       </Summary>
