f98677f35af616fef57b77a9605c6b0f1a2ddb37
marcos <dev.marcosanjos@gmail.com>
2025-01-16 11:38:58 -0300

    feat: Add summary section with total display and confirmation button to Cart component

--- app/src/components/Cart/index.tsx
+++ app/src/components/Cart/index.tsx
@@ -7,12 +7,15 @@ import {
   ProductContainer,
   ProductDetails,
   QuantityContainer,
+  Summary,
+  TotalContainer,
 } from "./style";
 import { Text } from "../Text";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
 import { CheckCircle } from "../Icons/CheckCircle";
 import { MinusCircle } from "../Icons/MinusCircle";
+import { Button } from "../Button";
 
 interface CartProps {
   cartItems: CartItem;
@@ -20,42 +23,60 @@ interface CartProps {
 
 export function Cart({ cartItems }: CartProps) {
   return (
-    <FlatList
-      data={cartItems}
-      keyExtractor={(cartItems) => cartItems.product._id}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item: cardItem }) => (
-        <Item>
-          <ProductContainer>
-            <Image
-              source={{
-                uri: `https://448a-186-194-148-5.ngrok-free.app/uploads/${cardItem.product.imagePath}`,
-              }}
-            />
-            <QuantityContainer>
-              <Text size={14} color="#666">
-                {cardItem.quantity} x
-              </Text>
-            </QuantityContainer>
-            <ProductDetails>
-              <Text size={14} weight="600">
-                {cardItem.product.name}
-              </Text>
-              <Text size={14} color="#666" style={{ marginTop: 4 }}>
-                {formatCurrency(cardItem.product.price)}
-              </Text>
-            </ProductDetails>
-          </ProductContainer>
-          <Action>
-            <TouchableOpacity style={{ marginRight: 24 }}>
-              <PlusCircle />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <MinusCircle />
-            </TouchableOpacity>
-          </Action>
-        </Item>
-      )}
-    />
+    <>
+      <FlatList
+        data={cartItems}
+        keyExtractor={(cartItems) => cartItems.product._id}
+        style={{
+          marginBottom: 20,
+          maxHeight: 150,
+        }}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item: cardItem }) => (
+          <Item>
+            <ProductContainer>
+              <Image
+                source={{
+                  uri: `https://448a-186-194-148-5.ngrok-free.app/uploads/${cardItem.product.imagePath}`,
+                }}
+              />
+              <QuantityContainer>
+                <Text size={14} color="#666">
+                  {cardItem.quantity} x
+                </Text>
+              </QuantityContainer>
+              <ProductDetails>
+                <Text size={14} weight="600">
+                  {cardItem.product.name}
+                </Text>
+                <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                  {formatCurrency(cardItem.product.price)}
+                </Text>
+              </ProductDetails>
+            </ProductContainer>
+            <Action>
+              <TouchableOpacity style={{ marginRight: 24 }}>
+                <PlusCircle />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <MinusCircle />
+              </TouchableOpacity>
+            </Action>
+          </Item>
+        )}
+      />
+      <Summary>
+        <TotalContainer>
+          <Text color="#666">Total</Text>
+          <Text size={20} weight="600">
+            {formatCurrency(120)}
+          </Text>
+        </TotalContainer>
+
+        <Button onPress={() => alert("Confimar pedido")}>
+          Confimar pedido
+        </Button>
+      </Summary>
+    </>
   );
 }
--- app/src/components/Cart/style.ts
+++ app/src/components/Cart/style.ts
@@ -25,3 +25,11 @@ export const QuantityContainer = styled.View`
 `;
 
 export const ProductDetails = styled.View``;
+
+export const Summary = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalContainer = styled.View``;
